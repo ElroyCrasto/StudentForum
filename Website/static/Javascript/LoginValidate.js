534fda7e6094f3501166c77c3c06c1f162505227
Leroy Moraes <lvmoraes223@gmail.com>
2023-07-02 14:14:44 +0530

    Add 'show password' feature

--- Website/static/Javascript/LoginValidate.js
+++ Website/static/Javascript/LoginValidate.js
@@ -1,15 +1,21 @@
 function LoginValidate(){
     var Form = document.getElementById('loginform');
     var Username = document.getElementById('username').value;
-    var Password = document.getElementById('password').value;
     var msg = document.getElementById('error');
     var regex = /\s+/;
-
 
     if (Username.match(regex) || Username == ''){
         msg.innerHTML="Please Enter Valid Username";
     }else{
         Form.submit()
     }
+}
 
+function ShowPassword(){
+    var Password = document.getElementById('password');
+    if (Password.type == "password"){
+        Password.type = "text";
+    }else{
+        Password.type = "password";
+    }
 }
