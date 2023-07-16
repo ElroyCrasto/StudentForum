d1ab8893d0e26402e7d02c3f3b7beb4b0b4cbc50
Faisal <faisalkhan200ns@gmail.com>
2023-07-16 13:35:02 +0530

    Add spinners and checkers

--- Website/AccountRoutes.py
+++ Website/AccountRoutes.py
@@ -1,3 +1,8 @@
-from flask import Blueprint
-
+from flask import Blueprint, render_template
+from flask_login import login_required
 AccountRoute = Blueprint("AccountRoute", __name__)
+
+@AccountRoute.route("/Profile")
+@login_required
+def ProfilePage():
+    return render_template("ProfilePage.html")
